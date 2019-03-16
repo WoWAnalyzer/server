270443b6501a8bc90689bc88d26cb01d91ad82ba
Martijn Hols <git@martijnhols.nl>
2019-03-16 22:34:33 +0100

    Disable Sentry handlers (does this cause memory leakage?)

--- src/configureRaven.js
+++ src/configureRaven.js
@@ -6,8 +6,8 @@ export default function configureSentry(app) {
       dsn: process.env.SENTRY_DSN,
     });
     // The Sentry request handler must be the first middleware on the app
-    app.use(Sentry.Handlers.requestHandler());
+    // app.use(Sentry.Handlers.requestHandler());
     // The error handler must be before any other error middleware
-    app.use(Sentry.Handlers.errorHandler());
+    // app.use(Sentry.Handlers.errorHandler());
   }
 }

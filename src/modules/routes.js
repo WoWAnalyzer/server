03263da8268cf531a5a22d5d152f91551e817db3
Martijn Hols <git@martijnhols.nl>
2019-03-17 13:09:11 +0100

    404 when an API endpoint doesn't exist

--- src/modules/routes.js
+++ src/modules/routes.js
@@ -8,6 +8,13 @@ router.use(require('./character').default);
 router.use(require('./item').default);
 router.use(require('./spell').default);
 router.use(require('./thirdparty').default);
+
+// When requesting an API endpoint that doesn't exist we shouldn't fallback to the app
+router.get('/i/*', function (req, res) {
+  console.log('API 404', req.originalUrl);
+  res.sendStatus(404);
+});
+
 router.use(require('./spa').default);
 
 export default router;

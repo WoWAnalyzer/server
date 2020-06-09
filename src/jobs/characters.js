6a264a4d0accb75bef033171ad95720ebdbc594e
cheezburgirl <cheezburgirl@gmail.com>
2020-06-09 22:55:49 +0200

    Wipe character data instead of removing it

--- src/jobs/characters.js
+++ src/jobs/characters.js
@@ -5,16 +5,62 @@ import { Op } from 'sequelize';
 
 const Character = models.Character;
 
-async function deleteExpiredCharacters() {
-  const expirationDate = moment().subtract(30, 'days').toDate();
-  console.log(`Deleting characters older than ${expirationDate}...`);
-
-  await Character.destroy({
+async function updateCharacter(character) {
+  await Character.update({
+    battlegroup: null,
+    faction: null,
+    class: null,
+    race: null,
+    gender: null,
+    achievementPoints: null,
+    thumbnail: null,
+    spec: null,
+    role: null,
+    talents: null,
+    heartOfAzeroth: null
+  }, {
     where: {
+      id: character.id
+    }
+  });
+}
+
+async function deleteExpiredCharacters() {
+  Character.findAll({
+    where: {
+      // we don't want to wipe data that is already wiped
+      [Op.or]: [
+        { battlegroup: { [Op.ne]: null } },
+        { faction: { [Op.ne]: null } },
+        { class: { [Op.ne]: null } },
+        { race: { [Op.ne]: null } },
+        { gender: { [Op.ne]: null } },
+        { achievementPoints: { [Op.ne]: null } },
+        { thumbnail: { [Op.ne]: null } },
+        { spec: { [Op.ne]: null } },
+        { role: { [Op.ne]: null } },
+        { talents: { [Op.ne]: null } },
+        { heartOfAzeroth: { [Op.ne]: null } },
+      ],
+      // we only want to wipe data after the TOS were changed
       blizzardUpdatedAt: {
-        [Op.lte]: expirationDate
+        [Op.between]: [moment.utc('2020-01-01'), moment().subtract(30, 'days').toDate()]
       }
     }
+  }).then(characters => {
+    console.log('Resetting', characters.length, 'characters');
+
+    let charactersModified = 0;
+    try {
+      characters.forEach(character => {
+        updateCharacter(character.dataValues);
+        charactersModified++;
+      });
+    } catch (error) {
+      Sentry.captureException(error);
+    }
+
+    console.log("Total Characters Nullified: " + charactersModified);
   });
 }
 

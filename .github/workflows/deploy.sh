a7d9ee38086612640dfdc299380ef3caa2ef62d9
Martijn Hols <git@martijnhols.nl>
2020-12-15 12:15:16 +0100

    Travis is refusing work so it gets fired and replaced with GitHub actions

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,17 +1,16 @@
 export REPO=wowanalyzer/wowanalyzer-server;
-export BUILD_TAG="build-$TRAVIS_BUILD_NUMBER";
 export DEPLOY_TAG=$(
-  if [ "$TRAVIS_BRANCH" == "master" ]; then
+  if [ "${GITHUB_REF##*/}" == "master" ]; then
     echo "latest";
   else
-    echo $TRAVIS_BRANCH;
+    echo ${GITHUB_REF##*/};
   fi | sed -r 's/\//-/g'
 );
 
 echo "> Create a Docker image for this specific build. This allows us to go back to a particular build at any time, and makes it possible to deploy without rebuilding by just re-tagging the image.";
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin;
-docker tag wowanalyzer-server $REPO:$BUILD_TAG;
-docker push $REPO:$BUILD_TAG;
-docker tag $REPO:$BUILD_TAG $REPO:$DEPLOY_TAG;
+docker tag wowanalyzer-server $REPO:$DOCKER_BUILD_TAG;
+docker push $REPO:$DOCKER_BUILD_TAG;
+docker tag $REPO:$DOCKER_BUILD_TAG $REPO:$DEPLOY_TAG;
 docker push $REPO:$DEPLOY_TAG;

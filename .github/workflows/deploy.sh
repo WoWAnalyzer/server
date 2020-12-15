ddd06283d77498a5bd69801c6183d6dbd8d01200
Martijn Hols <git@martijnhols.nl>
2020-12-15 13:02:24 +0100

    Revert "Docker logout before login"
    This reverts commit 0422638c6acf80450cfc2702675eba1b0dc0ceb9.

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -9,7 +9,6 @@ export DEPLOY_TAG=$(
 
 echo "> Create a Docker image for this specific build. This allows us to go back to a particular build at any time, and makes it possible to deploy without rebuilding by just re-tagging the image.";
 
-docker logout
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin;
 docker tag wowanalyzer-server $REPO:$DOCKER_BUILD_TAG;
 docker push $REPO:$DOCKER_BUILD_TAG;

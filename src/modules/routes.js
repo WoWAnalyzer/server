2f384c9e5c9847aa560828fab84e075d0b29e409
J David Smith <emallson@emallson.net>
2021-11-13 12:34:34 -0500

    add wcl ads.txt

--- src/modules/routes.js
+++ src/modules/routes.js
@@ -2,6 +2,7 @@ import Express from 'express';
 
 const router = Express.Router();
 
+router.use(require('./ads').default);
 router.use(require('./user').default);
 router.use(require('./wacraftlogs').default);
 router.use(require('./character').default);

f1b2478164f3a95f8c880efdf63120a9e0a385e4
emallson <emallson@emallson.net>
2024-04-12 18:27:16 -0400

    don't allow browsers to cache errors

--- src/common/cache-control.ts
+++ src/common/cache-control.ts
@@ -5,10 +5,15 @@ export const CACHE_ONE_WEEK = 604800;
 export const cacheControl = fp<{ cacheDuration?: number }>(
   async (app, options) => {
     app.addHook("onSend", async (_req, reply, payload) => {
-      reply.header(
-        "cache-control",
-        `max-age=${options.cacheDuration ?? CACHE_ONE_WEEK}`,
-      );
+      if (reply.statusCode >= 299) {
+        // do not cache errors or redirects
+        reply.header("cache-control", "no-cache");
+      } else {
+        reply.header(
+          "cache-control",
+          `max-age=${options.cacheDuration ?? CACHE_ONE_WEEK}`,
+        );
+      }
       return payload;
     });
   },
